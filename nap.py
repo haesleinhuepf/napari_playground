0d512bbf5981f298be56d8bcb2688fcec29137ab
haesleinhuepf <rhaase@mpi-cbg.de>
2019-10-02 18:33:40 +0200

    bugfix: create QT Application before viewing

--- nap.py
+++ nap.py
@@ -1,8 +1,11 @@
+
 from skimage import data
 import napari
 
-# create a Viewer and add an image
-viewer = napari.view_image(data.astronaut(), rgb=True)
+# create Qt GUI context
+with napari.gui_qt():
+    # create a Viewer and add an image
+    viewer = napari.view_image(data.astronaut(), rgb=True)
 
 import vispy
 print(vispy.sys_info())
